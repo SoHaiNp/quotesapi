cd334cb6ed717d8f13a92a6574b950d5a2ad9e2d
SoHaiNp <139929891+SoHaiNp@users.noreply.github.com>
2024-12-17 14:31:59 -0300

    Initial commit

---
+++ src/test/java/com/quotesapi/quotesapi/QuotesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quotesapi.quotesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
